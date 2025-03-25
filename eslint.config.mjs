25bde885af5bb7235d458c17261ae6630820df41
leogalisse <leonardogalisse@gmail.com>
2025-03-25 18:42:46 -0300

    style: add eslint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,10 @@
-import { defineConfig } from "eslint/config";
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -14,17 +14,18 @@ const compat = new FlatCompat({
 });
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
     rules: {
-      eqeqeq: "off",
-      "no-unused-vars": "error",
-      "prefer-const": ["error", { "ignoreReadBeforeAssign": true }],
-      semi: "error"
+      eqeqeq: 'off',
+      'no-unused-vars': 'error',
+      'prefer-const': ['error', { 'ignoreReadBeforeAssign': true }],
+      semi: 'error',
+      quotes: ['error', 'single']
     }
   }
 ]);
