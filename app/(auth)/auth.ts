ee836b702e912fd7a1c1b82f2042eedbad267be1
leogalisse <leonardogalisse@gmail.com>
2025-03-28 00:11:43 -0300

    feat: add auth to the application

---
+++ app/(auth)/auth.ts
@@ -0,0 +1,68 @@
+import { compare } from 'bcrypt-ts';
+import NextAuth, { type User, type Session } from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+import { getUser } from '@/lib/db/queries';
+import { authConfig } from './auth.config';
+import { type User as UserSchema } from '@/lib/db/schema';
+
+interface ExtendedSession extends Session {
+  user: User;
+}
+
+export const handler = NextAuth({
+  ...authConfig,
+  providers: [
+    Credentials({
+      credentials: {
+        email: { label: 'Email', type: 'text' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        if (!credentials || typeof credentials.email !== 'string' || typeof credentials.password !== 'string') {
+          console.error('Invalid credentials format:', credentials);
+          return null;
+        }
+
+        const users = await getUser(credentials.email);
+
+        if (users.length === 0) {
+          return null;
+        }
+
+        const passwordsMatch = await compare(credentials.password, users[0].password!);
+
+        if (!passwordsMatch) {
+          return null;
+        }
+
+        return users[0] as UserSchema;
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+
+      return token;
+    },
+    async session({ session, token }) {
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          id: token.id,
+        },
+      } as ExtendedSession;
+    },
+  },
+});
+
+const {
+  auth,
+  signIn,
+  signOut,
+} = handler;
+
+export { handler as GET, handler as POST, auth, signIn, signOut };
--- lib/env.ts
+++ lib/env.ts
@@ -2,7 +2,6 @@ import 'dotenv/config';
 import { z } from 'zod';
 
 const envSchema = z.object({
-  NODE_ENV: z.enum(['dev', 'test', 'prod']).default('dev'),
   DATABASE_URL: z.string(),
 });
 
